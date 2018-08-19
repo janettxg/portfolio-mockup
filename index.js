411dd716da5f0cde795d0ba0f2db1229b2893426
janett g <janettxgonzalez@gmail.com>
2018-08-19 00:57:21 -0400

    mockup portfolio

---
+++ index.js
@@ -0,0 +1,12 @@
+// jQuery
+var porfolioImage = $('#porfolio-img');
+
+$('#show-image')
+.click(function(){
+    porfolioImage.show();
+})
+
+$('#hide-image')
+.click(function(){
+    porfolioImage.hide();
+})
